27e81a0d7b37d8134110b65f5cadf6f4a71438be
varez05 <craacarlos6@gmail.com>
2025-05-01 21:45:56 -0500

    Prueba 4

--- Script/Script1.js
+++ Script/Script1.js
@@ -2,7 +2,25 @@ const cloud = document.getElementById("cloud"),
 barralateral = document.querySelector(".barra-lateral"),
 spans = document.querySelectorAll("Span"),
 palanca = document.querySelector(".switch"),
-circulo = document.querySelector(".circulo");
+circulo = document.querySelector(".circulo"),
+menu = document.querySelector(".menu"),
+main = document.querySelector("main");
+
+menu.addEventListener("click", () => {
+    barralateral.classList.toggle("max-barra-lateral");
+    if (barralateral.classList.contains("max-barra-lateral")) {
+        menu.children[0].style.display = "none";
+        menu.children[1].style.display = "block"; 
+    }else {
+        menu.children[0].style.display = "block";
+        menu.children[1].style.display = "none";          
+    }
+    if (window.innerWidth < 320) {
+        barralateral.classList.add("mini-barra-lateral");
+        main.classList.add("min-main");
+         spans.forEach((span) => {span.classList.add("oculto")});
+    }
+});
 
 palanca.addEventListener("click", () => {
     let body = document.body;
@@ -12,6 +30,7 @@ palanca.addEventListener("click", () => {
 
 cloud.addEventListener("click", () => {
     barralateral.classList.toggle("mini-barra-lateral");
+    main.classList.toggle("min-main");
     spans.forEach((span) => {
         span.classList.toggle("oculto")});
 });
