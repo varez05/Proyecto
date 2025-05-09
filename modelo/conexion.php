ba3b26584ad512dd46b0a51678193df7996daed8
breyner23 <barre0108@gmail.com>
2025-05-09 13:51:00 -0500

    Actualizar configuración de conexión a la base de datos

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$conexion = mysqli_connect("localhost", "root", "", "corporacion");
+$conexion = mysqli_connect("b8b6wjxwwgatbkzi3sc7-mysql.services.clever-cloud.com", "uvzy20bldxipuq8x", "cTXQO8Rz00laC0L5lFP8", "b8b6wjxwwgatbkzi3sc7");
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
