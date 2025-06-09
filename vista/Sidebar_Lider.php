a0dd8bb931dd7ce41c6761b5d0a0f6ad573d5e70
varez05 <craacarlos6@gmail.com>
2025-06-08 23:09:55 -0500

    Eliminar funcionalidad de modo oscuro y actualizar ruta de imagen de usuario en Sidebar_Lider.php

--- vista/Sidebar_Lider.php
+++ vista/Sidebar_Lider.php
@@ -78,24 +78,9 @@ if (!isset($_SESSION['usuario']) || $_SESSION['rol'] !== 'lider') {
             </ul> 
         </nav>
         <div>
-            <div class="linea">
-            </div>
-            <div class="modo-oscuro">
-                <div class="info">
-                    <ion-icon name="moon-outline"></ion-icon>
-                    <span> Modo Oscuro</span>
-                </div>
-                <div class="switch">
-                    <div class="base">
-                        <Div class="circulo">
-    
-                        </Div>
-                    </div>  
-                </div>
-            </div>
     
             <div class="usuario">
-                <img src="<?php echo isset($_SESSION['img']) ? '../imagen/' . $_SESSION['img'] : '../imagen/Fondo1.jpg'; ?>" alt="">
+                <img src="<?php echo isset($_SESSION['img']) ? '../uploads/' . $_SESSION['img'] : '../uploads/'; ?>" alt="">
                     <div class="info-usuario">
                         <div class="nombre-email">
                             <span class="nombre">
